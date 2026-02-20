240369e70a6411a0fd761305f989e86e6ad05b03
Lênin Müller Sampaio <lenin_muller@uol.com.br>
2026-02-20 14:10:22 -0300

    feat: ajuste de rota

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  api_id: "9t2mw7tno9",
-  apiUrl: 'https://9t2mw7tno9.execute-api.us-east-1.amazonaws.com/api/auth',
+  api_id: "ef39m2gyya",
+  apiUrl: 'https://ef39m2gyya.execute-api.us-east-1.amazonaws.com/api/auth',
   dashboardRemoteEntry: 'https://cdn-url/vyracare-app-dashboard-mfe/remoteEntry.js',
   userRemoteEntry: 'https://cdn-url/vyracare-app-user-mfe/remoteEntry.js'
 };
