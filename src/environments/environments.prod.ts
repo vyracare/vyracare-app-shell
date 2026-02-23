762f83f933df873e6caee14a9b5170fd33947203
Lênin Müller Sampaio <lenin_muller@uol.com.br>
2026-02-23 17:43:32 -0300

    feat: ajuste de rotas mfe user

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   api_id: "ef39m2gyya",
   apiUrl: 'https://ef39m2gyya.execute-api.us-east-1.amazonaws.com/api/auth',
   dashboardRemoteEntry: 'https://d3dtvolcohpeqn.cloudfront.net/remoteEntry.js',
-  userRemoteEntry: 'https://cdn-url/vyracare-app-user-mfe/remoteEntry.js'
+  userRemoteEntry: 'https://d2r6ze9ogurvf9.cloudfront.net/remoteEntry.js'
 };
