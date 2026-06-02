6063c02f65e0b796db90bd35b6fb0b0bcc8d4e88
Lênin Müller Sampaio <lenin_muller@uol.com.br>
2026-06-02 17:34:21 -0300

    feat: adiciona environment dev ao shell

---
+++ src/environments/environments.dev.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  dashboardRemoteEntry: 'http://localhost:4201/remoteEntry.js',
+  userRemoteEntry: 'http://localhost:4202/remoteEntry.js',
+  profileRemoteEntry: 'http://localhost:4203/remoteEntry.js',
+  proceedingsRemoteEntry: 'https://d2k3jf7r93hruy.cloudfront.net/remoteEntry.js',
+  api_id: "https://jkvfvgsw4l.execute-api.us-east-1.amazonaws.com",
+  apiUrl: 'https://jkvfvgsw4l.execute-api.us-east-1.amazonaws.com/api/auth'
+};
