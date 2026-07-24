0fc57a69989c4eb18760693bc33874c91f4d5185
Lênin Müller Sampaio <lenin_muller@uol.com.br>
2026-07-24 15:08:43 -0300

    corrige remotes de homologacao no shell

--- src/environments/environments.hml.ts
+++ src/environments/environments.hml.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   dashboardRemoteEntry: 'https://d3dtvolcohpeqn.cloudfront.net/remoteEntry.js',
-  userRemoteEntry: 'https://d2r6ze9ogurvf9.cloudfront.net/remoteEntry.js',
-  profileRemoteEntry: 'https://d3ko4u9he32ku6.cloudfront.net/remoteEntry.js',
-  proceedingsRemoteEntry: 'https://d2k3jf7r93hruy.cloudfront.net/remoteEntry.js',
+  userRemoteEntry: 'https://d3nh52q3cqgnr4.cloudfront.net/remoteEntry.js',
+  profileRemoteEntry: 'https://d35wbckhaythdp.cloudfront.net/remoteEntry.js',
+  proceedingsRemoteEntry: 'https://d2d4tgin9gsc7m.cloudfront.net/remoteEntry.js',
   apiUrl: 'https://jkvfvgsw4l.execute-api.us-east-1.amazonaws.com/api/auth'
 };
